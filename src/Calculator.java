50d7d726f7e0787e6327329eb425a5868b7d14cc
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-03-16 09:16:01 -0300

    computePolish method implemented

--- src/Calculator.java
+++ src/Calculator.java
@@ -64,8 +64,75 @@ public class Calculator {
 		 return s_parent;
 	 }
 	 
+	 public String getPolish (){
+		 computePolish();
+		 
+		 Iterator<Union> it = polish.iterator();
+		 String s_parent = "";
+		 
+		 while (it.hasNext())
+			 s_parent += it.next().toString() + " ";
+		 
+		 return s_parent;
+	 }
+	 
 	 public void clear (){
 		 parent.clear();
+	 }
+	 
+	 //Shunting-yard algorithm
+	 private void computePolish (){
+		 Iterator<Union> it = parent.iterator();
+		 Stack<Union> op_stack = new Stack<Union>();
+		 Union union;
+		 
+		 polish.clear();
+		 
+		 while (it.hasNext()){
+			 union = it.next();
+			 
+			 if (union.isNumber())
+				 polish.addLast (union);
+			 else {
+				 switch (union.getOp()){
+				 case SUM: case SUB:
+					 while (!op_stack.empty() &&
+							 op_stack.peek().getOp() != Op.OPP)
+						 polish.addLast (op_stack.pop());
+					 
+					 op_stack.push(union);
+					 break;
+					 
+				 case MUL: case DIV:
+					 while (!op_stack.empty() &&
+							 op_stack.peek().getOp() != Op.SUM &&
+							 op_stack.peek().getOp() != Op.SUB &&
+							 op_stack.peek().getOp() != Op.OPP)
+						 polish.addLast(op_stack.pop());
+					 
+					 op_stack.push(union);
+					 break;
+					 
+				 case POW: case OPP:
+					 op_stack.push(union);
+					 break;
+					 
+				 case CLP:
+					 while (op_stack.peek().getOp() != Op.OPP)
+						 polish.addLast(op_stack.pop());
+					 
+					 op_stack.pop();
+					 break;
+				 }
+			 }
+		 }
+		 
+		 while (!op_stack.empty())
+			 polish.addLast(op_stack.pop());
+	 }
+	 
+	 public void delete (){
+		 parent.removeLast();
 	 }
 	 
 	 public void insert (Op op){
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,22 @@ public class Main {
 		//testing Calculator methods
 		Calculator calc = new Calculator();
 		
-		calc.insert(1);
+		calc.insert(3);
 		calc.insert(Op.SUM);
+		calc.insert(4);
+		calc.insert(Op.MUL);
 		calc.insert(2);
-		System.out.println(calc.getParent());
+		calc.insert(Op.DIV);
+		calc.insert(Op.OPP);
+		calc.insert(1);
+		calc.insert(Op.SUB);
+		calc.insert(5);
+		calc.insert(Op.CLP);
+		calc.insert(Op.POW);
+		calc.insert(2);
+		calc.insert(Op.POW);
+		calc.insert(3);
+		System.out.println(calc.getPolish());
 		
 		calc.clear();
 		
@@ -18,7 +30,7 @@ public class Main {
 		calc.insert(Op.SUM);
 		calc.insert(5);
 		calc.insert(Op.CLP);
-		System.out.println(calc.getParent());
+		System.out.println(calc.getPolish());
 		
 	}
 }
